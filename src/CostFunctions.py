cce4a25cfee8a4828dfd671988b1aebcbae6ba87
Felix Kleine Bösing <felix,boesing@t-online.de>
2019-06-01 14:35:04 +0200

    Update costFunction.py
    added missing whitespace

--- src/CostFunctions.py
+++ src/CostFunctions.py
@@ -2,5 +2,5 @@ import numpy as np
 
 
 def rmse(preds: np.ndarray, actuals: np.ndarray):
-    rmse = np.sqrt(np.mean((preds - actuals)** 2))
+    rmse = np.sqrt(np.mean((preds - actuals) ** 2))
     return rmse
